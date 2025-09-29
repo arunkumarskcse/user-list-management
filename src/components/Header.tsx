3df4e712593a76fc76d01430cb90facb35560ca3
Arunk <arunkumarskcse@gmail.com>
2025-09-29 09:38:05 +0530

    navigate issue in render

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import { LogoutOutlined } from '@ant-design/icons';
 import { Avatar, Layout, message, Space, Typography } from 'antd';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const HeaderComponent = () => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const { Header } = Layout;
     const { Title } = Typography;
@@ -15,10 +15,11 @@ const HeaderComponent = () => {
     const Logout = () => {
         localStorage.removeItem('token');
         messageApi.success('Logged out successfully!');
-        setTimeout(() => {
-            navigate('/');
-        }, 2000);
+        // setTimeout(() => {
+        //     navigate('/');
+        // }, 2000);
         // window.location.reload();
+        window.location.href = '/';
     }
 
     return (
